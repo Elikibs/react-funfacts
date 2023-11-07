4b9934d436fd8bf711e101e38d77fa2bee2ea7e3
Elisha <elishakibet67@gmail.com>
2023-11-07 18:52:19 +0300

    Update of main file

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ function Main(){
         <main>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
-                <li>Was fisrt released in 2013</li>
+                <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
